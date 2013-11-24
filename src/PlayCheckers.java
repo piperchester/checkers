10dfc48e12351533ee5fb0be33a894a37ad15c97
Piper Chester <piperchester@gmail.com>
2013-11-24 12:53:14 -0500

    Clean up PlayCheckers.

--- src/PlayCheckers.java
+++ src/PlayCheckers.java
@@ -1,50 +1,31 @@
-/*
-
- * PlayCheckers.java
-
+/**
+ * PlayCheckers.java 
  *
-
- * 
-
  * Version:
-
  *   $Id: PlayCheckers.java,v 1.1 2002/10/22 21:12:53 se362 Exp $
-
- *
-
+ *   
  * Revisions:
-
  *   $Log: PlayCheckers.java,v $
  *   Revision 1.1  2002/10/22 21:12:53  se362
  *   Initial creation of case study
- *
  */
-
 
 /**
  *
- * The main class to run the game. It creates the driver and first screen
+ * The main class to run the game. It creates driver and first screen.
  *
  * @author
  *
  */
 class PlayCheckers{
 	
-	/*
-	* The main method to play checkers
-	*
-	*@param args[] the command line arguments
+	/**
+	* The main method to play checkers.
 	* 
 	*/
-	
     public static void main( String args[] ){
-	
-	
-	Driver theDriver = new Driver();
-	
-	Firstscreen first = new Firstscreen( theDriver.getFacade() );
-	first.show();
-	
-    }
-    
-}//PlayCheckers
+		Driver theDriver = new Driver();
+		Firstscreen first = new Firstscreen( theDriver.getFacade() );
+		first.show(); // Piper - Deprecated.
+    }   
+}
