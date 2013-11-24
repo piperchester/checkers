40bdf1075719c809054110591f7470a8dd175e35
Piper Chester <piperchester@gmail.com>
2013-11-24 13:13:07 -0500

    Clean up Board.

--- src/Board.java
+++ src/Board.java
@@ -13,10 +13,8 @@
 import java.util.*;
 import java.awt.*;
 
-
 /**
- *  This class represents the board on which checkers is being played.
- *  The board holds a collection of pieces.
+ *  The board. Holds a collection of pieces.
  *
  *  @invariant all variables have valid values
  *
@@ -30,11 +28,9 @@ public class Board {
 
 
    /**
-    * This constructor creates a new board at the beginning of the game
+    * Creates a new board at the beginning of the game.
     */
-
    public Board() {
-  
 	   // create a array of size 64, generate piece objects and
 	   // put them in the correct location in the array
 	   // Set the values of numWhites and numBlues to 12 each
@@ -67,10 +63,7 @@ public class Board {
 	   pieces[58] = new SinglePiece( Color.white );
 	   pieces[60] = new SinglePiece( Color.white );
 	   pieces[62] = new SinglePiece( Color.white );
-
    }
-
-   
 
    /**
     * Move the piece at the start position to the end position
@@ -81,188 +74,135 @@ public class Board {
     * @return -1 if there is a piece in the end position
     */
    public int movePiece(int start, int end) {
-
-
+	   
 	   int returnValue = 0;	
 
 	   // check if the end position of the piece is occupied
-	   if( occupied( end ) ) {
-	   	
-	   	   // if it is return -1
-	   	   returnValue = -1;
-
-
-	   // if it is not set a start position in the array to null
+	   // if not, set a start position in the array to null
 	   // put a piece object at the end position in the array
 	   // that was at the start positon before
+	   if( occupied( end ) ) {
+	   	   returnValue = -1;
 	   } else {
-	   	
 		   pieces[end] = pieces[start];
 		   pieces[start] = null;
-
-
 	   }
-
+	   
 	   return returnValue;
-
    }
 
-   
-
    /**
-    * This method checks if the space on the board contains a piece
+    * Checks if the space on the board contains a piece. If outside board
+    * array bounds, return true.
     * 
     * @param space - the space that needs to be checked
     * 
-    * @return true or false depending on the situation
+    * @return true if space is occupied, false otherwise
     */
    public boolean occupied(int space) {
 
 	   boolean returnValue = true;
-
-	   // if it's in the bounds of the array,
-	   	   // return true if the space is occupied
-	   	   // false if the space is empty
-	   // if it's outside the bounds of the array,
-	   	   // return true
            
 	   if ( space >= 1 && space <= 63 && pieces[space] == null ) {
 	   	   returnValue = false;
 	   }
 	   
-	   return returnValue;
-	   
+	   return returnValue;  
    }
 
    
    /**
-    * This method removes piece at the position space
+    * Removes piece at provided position space. Set piece to null.
     * 
     * @param space - the positon of the piece to be removed
     */
    public void removePiece(int space) {
-   
-	   // go to the space position in the array
-	   // set it equal to null
-	   
 	   pieces[ space ] = null;
-
    }
    
    
    /**
-    * This method creates a king piece 
+    * Creates a king piece. Adds new king to array of pieces. 
     * 
-    * @param space - the psotion at which the king piece is created 
+    * @param space - the position at which the king piece is created 
     */
    public void kingPiece(int space) {
-   
-	   // create a new king piece
-	   // go to the space position in the array and place it there
-	   // if the position is not ocupied
 	   Color color = pieces[space].getColor();
 	   Piece piece = new KingPiece( color );
-	   pieces[space] = piece;
-	   
+	   pieces[space] = piece;   
    }
    
    
    /**
-    * This method returns the color of the piece at a certain space
+    * Returns the color of the piece at a certain space. If no piece
+    * exists at space in board, reutrn null.
     * 
     * @param space - the position of the piece on the board
     * 
-    * @return the color of this piece
+    * @return the color of this piece if piece exists, else null.
     */
    public Color colorAt(int space) {
-	   
+	 
 	   Color returnValue = null;
-	   // go to the space position in the array
-	   // check if there is a piece at that position
-	   // if there is none, return null
-	   // else retrun the color of the piece
 	   
 	   if( occupied( space ) ) {
-		   
-		   returnValue = pieces[space].getColor();
-		   
+		   returnValue = pieces[space].getColor(); 
 	   }
    
-       return returnValue;
-	   
+       return returnValue; 
    }
    
 
    /**
-    * This method returns the piece at the certain position
+    * Returns the piece at the certain position.
     * 
     * @param space - the space of the piece
     * 
-    * @return the piece at that space
+    * @return piece if it exists at that space.
     */
    public Piece getPieceAt(int space) {
 
 	   Piece returnValue = new SinglePiece(Color.red);
 	   
 	   try{
-	   	   // check if there is piece at space position
-	   	   // if there is none, return null
-	   	   // else return the piece at that position
-	   
 	      if( occupied(  space ) ) {
-		   
 	   	   	   returnValue = pieces[space];
-                           
 	   	   }
 	   
 	   } catch( ArrayIndexOutOfBoundsException e ) {
-	   
                           
 	   } catch( NullPointerException e ) {
 	   	
-           
 	   }
 	   
       return returnValue;
-	   
    }
    
-   
-   
    /**
-    * This method returns if there is a piece of color on the board
+    * Returns if there is a piece of color on the board.
     * 
     * @param color - the color of the piece
     * 
-    * @return true if there is a piece of color left on the board
-    *				else return false	
+    * @return true if a piece of color left on the board, else false
     */
    public boolean hasPieceOf( Color color) {
    	
-
 	   boolean returnValue = false;
 
-	   // go through the whole array
-	   // if there is a piece of color in the arrar return true
-	   // else return false
-	   for( int i =1; i < pieces.length; i++ ) {
+	   for( int i = 1; i < pieces.length; i++ ) {
 		   
 	   	   if( pieces[i] != null && pieces[i].getColor() == color ) {
-
-                  
 	   	   	   	   returnValue = true;
 	   	   	   	   i = pieces.length;
 	   	   	   }
 	   }
-
            
-      return returnValue;
-	   
+      return returnValue; 
    }
    
 
    /**
-    * This method returns the size of the board
+    * Returns the size of the board.
     * 
     * @return the size of the board, always 64
     */
@@ -270,10 +210,8 @@ public class Board {
        return 64;
    }
    
-   
-   
    /**
-    * This method returns a vector containing all blue Pieces
+    * Returns a vector containing all blue Pieces.
     * 
     * @return blue pieces on the board
     */
@@ -289,13 +227,12 @@ public class Board {
           }                 
       }
       
-      return bluePieces;
-      
+      return bluePieces; 
   }
  
  
    /**
-    * This method returns a vector containing all white Pieces
+    * Returns a vector containing all white Pieces.
     * 
     * @return white pieces on the board
     */
@@ -313,6 +250,4 @@ public class Board {
       
       return whitePieces;
  }
- 
-}//Board
-
+}
