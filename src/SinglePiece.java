91265fe62103c9ca1db5d120e208db2e5e174829
Piper Chester <piperchester@gmail.com>
2013-11-24 12:51:02 -0500

    Clean up SinglePiece.

--- src/SinglePiece.java
+++ src/SinglePiece.java
@@ -15,39 +15,32 @@ import java.awt.*;
 import java.util.*;
 
 /**
- * This is a class representing a single piece (a piece that has not been
- * kinged yet)
- *
- * @author
+ * Represents a piece that has not been kinged yet.
  *
  */
 public class SinglePiece extends Piece {
-	
-	
+
    private static int SINGLE = 0; // this is a single type
    private int type; // the type of the piece
    
    /**
-    * This constructor creates a single piece checker object
+    * Creates a single piece checker object.
     * 
     * @param c - the color of this single piece
     * @param pc - the possible captures of this single piece
     * @param pm - the possible moves of this single piece
     */
    public SinglePiece( Color c  ) {
- 
 	    super( c );
 		type = SINGLE;
    }
    
    /**
-    * This method returns that the type of the checker is single
+    * Returns that the type of the checker is single.
     * 
     * @return type which is 0 for single
     */
    public int getType() {
- 
 	   return type;
-   }
-   
-}// SinglePiece
+   }  
+}
