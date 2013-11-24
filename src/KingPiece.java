afae523a748ad29fd2aa6d0e86f8f11efa21c4be
Piper Chester <piperchester@gmail.com>
2013-11-24 14:23:57 -0500

    Clean up KingPiece.

--- src/KingPiece.java
+++ src/KingPiece.java
@@ -11,40 +11,31 @@
  *
  */
 import java.awt.*;
-import java.util.*;
 
 /**
- * This is a class representing a king piece (a piece that has been kinged)
- *
- * @author
+ * Represents a king piece.
  *
  */
 public class KingPiece extends Piece {
-
-   private static int KING = 1; // the king type
-   private int type; // the type of his object
+   private static int KING = 1;
+   private int type; 
   
    /**
     * This constructor creates a king piece object
     * 
-    * @param c - the color of this king piece
-    * @param pc - the possible captures of this king piece
-    * @param pm - the possible moves of this king piece
+    * @param kingColor - the color of this king piece
     */
-   public KingPiece( Color c ) {
-   
-	   super( c ); 
+   public KingPiece( Color kingColor ) {
+	   super( kingColor ); 
 	   type = KING;
    }
    
    /**
-    * This method returns the type of piece that this object is 
+    * Returns the type of piece that this object is.
     * 
     * @return 1 for the king piece representation
     */
    public int getType() {
-  
 	   return type;
-   }
-   
-}//KingPiece
+   }  
+}
