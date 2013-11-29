0d399c3e81576e75c463aeb8769c985c83ec32ce
Piper Chester <piperchester@gmail.com>
2013-11-29 12:03:55 -0500

    Add ICommand.

---
+++ src/ICommand.java
@@ -0,0 +1,12 @@
+/** 
+ * Governs methods implemented in all concrete command
+ * classes for the command pattern.
+ * 
+ * @author piperchester
+ */
+public interface ICommand {
+	/**
+	 * A method to carry out the command.
+	 */
+	public void execute();
+}
