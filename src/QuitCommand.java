0da977d8e8c4b3034ed801181872046331d7a002
Piper Chester <piperchester@gmail.com>
2013-11-29 12:37:50 -0500

    Add skeleton for QuitCommand.

---
+++ src/QuitCommand.java
@@ -0,0 +1,37 @@
+import javax.swing.JOptionPane;
+
+/**
+ * Provides GUI with the necessary data to acquire the next image in the study.  If there are 
+ * no more images, the command stops execution before notifying the 
+ * receiver.  This class also uses the GUI���s current view to determine 
+ * whether to move by increments of one or four.
+ * 
+ * @author piperchester
+ */
+public class QuitCommand implements ICommand {
+
+	private CheckerGUI receiver;
+	
+	/**
+	 * Single Arg constructor provides the receiver for the command
+	 * @param receiver the recipient for this command's actions.
+	 */
+	public QuitCommand(CheckerGUI receiver) {
+		this.receiver = receiver;
+	}
+	
+	/**
+	 * Handles logic associated with the Quitting command.
+	 */
+	@Override
+	public void execute() {
+		
+		JOptionPane.showMessageDialog( null, "Game has ended because: " /* player name */, "Game Over", 
+				JOptionPane.INFORMATION_MESSAGE );
+		
+		System.exit(0);
+
+		// Update the receiver with the new values
+		// receiver.setImageIndexing(interval, index);
+	}
+}
