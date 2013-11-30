0123908fe979749abf08bcab13f86af2478751de
Piper Chester <piperchester@gmail.com>
2013-11-30 10:32:02 -0500

    Minor clean up of PlayCheckers.

--- src/PlayCheckers.java
+++ src/PlayCheckers.java
@@ -1,32 +1,17 @@
 /**
- * PlayCheckers.java 
- *
- * Version:
- *   $Id: PlayCheckers.java,v 1.1 2002/10/22 21:12:53 se362 Exp $
- *   
- * Revisions:
- *   $Log: PlayCheckers.java,v $
- *   Revision 1.1  2002/10/22 21:12:53  se362
- *   Initial creation of case study
- */
-
-/**
- *
- * The main class to run the game. It creates driver and first screen.
- *
- * @author
+ * The main class to run the game. Creates driver and first screen.
  *
  */
 class PlayCheckers{
 	
 	/**
-	* The main method to play checkers.
-	* 
+	* The main method to play checkers. Instantiate
+	* IMediator, Driver, and InitialScreen.
 	*/
-    public static void main( String args[] ){
+	public static void main( String args[] ){
     	IMediator mediator = new GameplayMediator();
 		Driver theDriver = new Driver(mediator);
 		InitialScreen second = new InitialScreen(theDriver,  theDriver.getMediator());
-		second.show(); // Piper - Deprecated.
+		second.setVisible(true);
     }   
 }
