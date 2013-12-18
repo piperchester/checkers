eafd880396da3f383560297a08e6c81b1f6953ef
Piper Chester <piperchester@gmail.com>
2013-12-17 21:30:55 -0500

    Minor styling of SinglePiece.

--- src/SinglePiece.java
+++ src/SinglePiece.java
@@ -1,24 +1,21 @@
-import java.awt.*;
+import java.awt.Color;
 
 /**
  * Represents a piece that has not been kinged yet.
- *
  */
 public class SinglePiece extends Piece {
 
-   private static int SINGLE = 0; // this is a single type
+   private static int SINGLE_PIECE = 0; // this is a single type
    private int type; // the type of the piece
    
    /**
     * Creates a single piece checker object.
     * 
     * @param c - the color of this single piece
-    * @param pc - the possible captures of this single piece
-    * @param pm - the possible moves of this single piece
     */
-   public SinglePiece( Color c  ) {
-	    super( c );
-		type = SINGLE;
+   public SinglePiece(Color pieceColor) {
+	    super(pieceColor);
+		type = SINGLE_PIECE;
    }
    
    /**
